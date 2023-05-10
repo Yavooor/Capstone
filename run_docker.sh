e48cbbc1c90c00efb9f6df4f3697ea6a7d69d1bf
Yavor Mihalski <yavor.mihalski@tui.com>
2023-05-10 09:02:02 +0000

    run docker

--- run_docker.sh
+++ run_docker.sh
@@ -5,8 +5,6 @@ docker image ls
 
 docker tag capstone:latest 018538312056.dkr.ecr.us-east-1.amazonaws.com/capstone:latest 
 
-docker run -p 8000:80 capstone
-
 docker push 018538312056.dkr.ecr.us-east-1.amazonaws.com/capstone:latest
 
 
